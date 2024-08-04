466ba2b5f0f6353df5567ca41c459129fe411436
Samim Piyada <165650510+psamim7662@users.noreply.github.com>
2024-08-04 20:11:35 +0530

    Add files via upload

---
+++ search_word_and_Replace_with_a_substring.py
@@ -0,0 +1,13 @@
+def modifystring(string,string1,string2):
+    #store the resultant string 
+    ans=""
+
+    #traverse through string 
+    i=0
+    i<len(string):
+    if string==string1[k] and i+len(string1)<=len(s):
+        j=i
+        j<i+len(string1) and string[i]==string1[k]:
+        k+=1
+        j+=1
+        
